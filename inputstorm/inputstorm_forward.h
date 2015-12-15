2f0e18bb38e6af0c40a077fd205988d4c4dbe7f8
SlowRiot <rain.backnet@gmail.com>
2015-12-15 07:03:52 +0000

    adding forward declaration header

---
+++ inputstorm/inputstorm_forward.h
@@ -0,0 +1,21 @@
+#ifndef INPUTSTORM_FORWARD_H_INCLUDED
+#define INPUTSTORM_FORWARD_H_INCLUDED
+
+namespace inputstorm {
+
+class manager;
+
+namespace input {
+
+struct cursor;
+struct joystick;
+struct joystick_axis_bindingtype;
+struct key;
+struct mousebutton;
+struct scroll;
+struct text;
+
+}
+}
+
+#endif // INPUTSTORM_FORWARD_H_INCLUDED
