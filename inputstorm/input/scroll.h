c482b609938af2deab6013ad57c8fdfef63f33ea
Borealis <slowriot@voxelstorm.com>
2016-09-07 21:25:56 +0100

    Auto-aligning comments

--- inputstorm/input/scroll.h
+++ inputstorm/input/scroll.h
@@ -7,32 +7,3 @@
 namespace inputstorm {
 namespace input {
 
-struct scroll {
-  // types
-  enum class direction : char {
-    RIGHT,
-    LEFT,
-    UP,
-    DOWN,
-    END,
-    LAST = END - 1
-  };
-
-  // data
-private:
-  std::function<void(vec2d const&)> binding = [](vec2d const &change __attribute__((__unused__))){}; // callback function for scroll
-
-public:
-  std::string get_name(direction this_direction) const;
-
-  void bind(std::function<void(vec2d const&)> func);
-
-  void unbind();
-
-  void execute(vec2d const &offset);
-};
-
-}
-}
-
-#endif // INPUTSTORM_INPUT_SCROLL_H_INCLUDED
